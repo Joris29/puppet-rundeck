3a752f94b009d8da62df2a8f56bc27110aea9026
Joris29 <joris.vanacoleyen@bryxx.eu>
2026-06-17 11:30:49 +0200

    Remove redundant acceptance test and test upgrade to lates version

--- spec/acceptance/rundeck_spec.rb
+++ spec/acceptance/rundeck_spec.rb
@@ -11,7 +11,7 @@ describe 'rundeck class' do
           distribution => 'jre',
         }
         class { 'rundeck':
-          package_ensure => '5.0.2.20240212-1',
+          package_ensure => '5.7.0.20241021-1',
         }
         Class['java'] -> Class['rundeck']
         PUPPET
@@ -27,33 +27,12 @@ describe 'rundeck class' do
     end
   end
 
-  context 'updrade to 5.2.0.20240410-1' do
+  context 'upgrade to 5.20.1.20260518-1' do
     it_behaves_like 'an idempotent resource' do
       let(:manifest) do
         <<-PUPPET
          class { 'rundeck':
-          package_ensure => '5.2.0.20240410-1',
-        }
-        PUPPET
-      end
-    end
-
-    describe package('rundeck') do
-      it { is_expected.to be_installed }
-    end
-
-    describe service('rundeckd') do
-      it { is_expected.to be_enabled }
-      it { is_expected.to be_running }
-    end
-  end
-
-  context 'updrade to 5.7.0.20241021-1' do
-    it_behaves_like 'an idempotent resource' do
-      let(:manifest) do
-        <<-PUPPET
-         class { 'rundeck':
-          package_ensure => '5.7.0.20241021-1',
+          package_ensure => '5.20.1.20260518-1',
         }
         PUPPET
       end
